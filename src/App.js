889122e8b64dc56e4ad2562bdeecbb2bbacc6db0
ObianuoObi <117560960+ObianuoObi@users.noreply.github.com>
2023-03-10 07:38:38 +0000

    componenets folder and header.js file created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 
 function App() {
-  return 
+  return <>
+
+<header>
+  <h3>LOGO</h3>
+  <nav>
+    <a href="#">Home</a>
+    <a href="#">Projects</a>
+    <a href="#">Contact</a>
+  </nav>
+</header>
+
+<main>
+  <h1>Welcome to my Portfolio</h1>
+  <p> I'm half way there! The aim is to land a job as a Web developer & I'm right on track!</p>
+</main>
+    
+  </>
 }
 
 export default App;
