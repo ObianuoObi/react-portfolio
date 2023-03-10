c4133338725b23d2de11575bfa1a85d08dd086ac
ObianuoObi <117560960+ObianuoObi@users.noreply.github.com>
2023-03-10 07:56:16 +0000

    Home componet created and updated to App.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,7 @@ function App() {
 
 
 
-<main>
-  <h1>Welcome to my Portfolio</h1>
-  <p> I'm half way there! The aim is to land a job as a Web developer & I'm right on track!</p>
-</main>
+
     
   </>
 }
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+function Home () {
+    return (
+        <main>
+  <h1>Welcome to my Portfolio</h1>
+  <p> I'm half way there! The aim is to land a job as a Web developer & I'm right on track!</p>
+</main>
+    )
+}
+
+export default Home;
