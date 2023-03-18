1fbab5d8f4e222984765b6d7da7e7b6c1ff58a62
ObianuoObi <117560960+ObianuoObi@users.noreply.github.com>
2023-03-18 10:07:11 +0000

    Tailwind CSS installation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import Projects from './pages/Projects';
+
 
 
 function App() {
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,13 +1,48 @@
+import { useState } from "react";
+
+const styles = {
+    header: {
+        textAlign: 'center',
+        margin: '20px 0'
+    },
+
+    submitBtn: {
+       backgroundColor: 'lightseagreen',
+       color: '#fff',
+       cursor: 'pointer'
+    }
+};
+
 function Contact () {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        meaasge: ''
+    })
+
+    const handleChange = (event) => {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value
+        });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        console.log(formData);
+
+    };
+
     return (
     <> 
-    <h1>Contact</h1>
+    <h1 style={styles.header}>Contact</h1>
 
-    <form>
-        <input type='text' placeholder="Enter your Name"/>
-        <input type='text' placeholder="Enter your Email Address"/>
-        <textarea placeholder="Enter your message" cols= "30" rows= "10"></textarea>
-        <button>Submit</button>
+    <form onSubmit={handleSubmit}>
+        <input name= "name "onChange= {handleChange} type='text' placeholder="Enter your Name"/>
+        <input name= "email "onChange={handleChange} type='text' placeholder="Enter your Email Address"/>
+        <textarea name= "messasge "onChange={handleChange} placeholder="Enter your message..." cols= "30" rows= "10"></textarea>
+        <button style= {styles.submitBtn}>Submit</button>
     </form>
     </>
           
